4fd7af59aad193a05bdbdf76fffbcceb531a36a5
Adit Pankajbhai Lad <ladadit05@gmail.com>
2026-02-05 15:55:38 +0530

    Testing Production Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const sequelize = require('./config/database');
 
 const PORT = 5000;
-
+// Sync database and start server
 sequelize.sync().then(() => {
   console.log('Database connected');
   app.listen(PORT, () => {
